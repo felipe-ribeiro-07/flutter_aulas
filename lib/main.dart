8fabc0261b4c1ba008f2a808d9bc7dc152d42131
SaraNagahara <sarasfnagahara@gmail.com>
2026-06-09 10:32:08 -0300

    subindo minha pagina seja oque Deus quiser

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:ifood/cart_screen.dart';
 import 'package:ifood/home_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: HomeScreen(),
+      home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
+
+      routes: {
+        '/cart': (context) => const CartScreen(), 
+      },
 
     )
   );
